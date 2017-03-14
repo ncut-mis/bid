1dde0b0f1ea840f004b03f8e4ec9c70e10ad8a7f
KAIYULIN <ken91165@gmail.com>
2017-03-14 11:09:31 +0800

    新增wishlist資料表

---
+++ database/migrations/2017_03_14_021156_create_wishlist_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWishlistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wishlist', function (Blueprint $table) {
+           $table->increments('wish_id');
+            $table->integer('id');
+            $table->integer('aucyion_no');
+            $table->integer('quantity');
+            $table->timestamps();
+            $table->softDeletes();            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wishlist');
+    }
+}
--- resources/views/Account/account.blade.php
+++ resources/views/Account/account.blade.php
@@ -2,48 +2,28 @@
 
 @section('content')
  @include('common.errors')
-				<div class="row">
-					<div class="col-md-3">
-					<ul id="sidebar-nav" class="sidebar-nav margin-bottom-30px">
-					<nav class="navbar navbar-default ">   
- <div id="sidebar-wrapper">
-            <ul class="sidebar-nav-tabs">
-                <li class="sidebar-brand">
-                    <a href="#">
-                        Start Bootstrap
-                    </a>
-                </li>
-                <li>
-                    <a href="#">Dashboard</a>
-                </li>
-                <li>
-                    <a href="#">Shortcuts</a>
-                </li>
-                <li>
-                    <a href="#">Overview</a>
-                </li>
-                <li>
-                    <a href="#">Events</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-		
-                    </nav>
-					</ul>
-					</div>
-					
-					<div class="col-md-9">
+<div class="row">
+<div class="col-md-3 ">
+<ul class="sidebar-default ">
+<li class="list-group-item ">
+<a href="#Buttons" class="submenu-toggle" data-toggle="collapse" data-parent="sidebar-nav">會員資料</a>
+<ul id="Buttons" class="list-unstyled collapse submenu in">
+<li><a href="user_edit.php">資料修改</a></li>
+<li><a href="pwd_edit.php">密碼修改</a></li>
+<li><a href="address_edit.php">收貨地址</a></li>
+</ul>
+</li>
+<li class="list-group-item"><a href="tprecordlist.php">奪寶紀錄</a></li>				
+<li class="list-group-item"><a href="addmoney_record.php">儲值紀錄</a></li>
+<li class="list-group-item "><a href="tcrecordlist.php">獲得商品</a></li>
+<li class="list-group-item "><a href="win_share.php">贏家分享</a></li>
+<li class="list-group-item "><a href="{{ url('/logout') }}"onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                            Logout</a></li>
+</ul>
+</div>					
+<div class="col-md-9">
 						<!-- BASIC -->
-						<h2 class="section-heading">會員中心</h2>					
+ <h2 class="section-heading">會員中心</h2>					
 							<div class="row">
 								<div class="col-md-12">
 									<div class="team-member media">
@@ -56,4 +36,5 @@
 							</div>
 						<!-- END SIZING -->
 					</div>
+</div>
 @endsection
